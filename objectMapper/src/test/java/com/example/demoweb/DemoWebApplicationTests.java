a1d2e5379c9aaae4040ceeaf9bc110917582245d
Clean314 <pipie314@protonmail.com>
2022-12-27 02:40:39 +0900

    object mapper

--- objectMapper/src/test/java/com/example/demoweb/DemoWebApplicationTests.java
+++ objectMapper/src/test/java/com/example/demoweb/DemoWebApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demoweb;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,8 +9,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoWebApplicationTests {
 
 	@Test
-	void contextLoads() {
-		System.out.println("--------------------------");
+	void contextLoads() throws JsonProcessingException {
+		System.out.println("------------- [Object Mapper] -------------");
+		var objectMapper = new ObjectMapper();
+
+		// object mapper 가 object -> text 변환 시, get 메소드를 참조한다.
+		var user = new User("steve", 19, "010-0000-1111");
+		var text = objectMapper.writeValueAsString(user);
+		System.out.println(text);
+
+
+		// text -> object 변환 시에는 default 생성자를 참조
+		var objectUser = objectMapper.readValue(text, User.class);
+		System.out.println(objectUser);
 	}
 
 }
