b667ed1b0dc704603b6d8a13b2ef31bd1f4cfb71
Clean314 <pipie314@protonmail.com>
2023-01-13 02:51:04 +0900

    exception when validations in controller

--- Spring/Spring_usage/Exception/src/main/java/com/example/demoweb/advice/ExApiControllerAdvice.java
+++ Spring/Spring_usage/Exception/src/main/java/com/example/demoweb/advice/ExApiControllerAdvice.java
@@ -1,7 +1,9 @@
 package com.example.demoweb.advice;
 
 
+import com.example.demoweb.dto.Error;
 import com.example.demoweb.controller.ExApiController;
+import com.example.demoweb.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -11,7 +13,14 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 // ExApiController 에서만 동작하는 Advice 를 만들 수 있다. Global 보다 우선되는 듯
 @RestControllerAdvice(basePackageClasses = ExApiController.class)
@@ -25,25 +34,82 @@ public class ExApiControllerAdvice {
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest httpServletRequest){
+
+        List<Error> errorList = new ArrayList<>();
+
         BindingResult bindingResult = e.getBindingResult();
 
         bindingResult.getAllErrors().forEach(error -> {
             FieldError fieldError = (FieldError) error;
+
+            Error error1 = new Error();
+            error1.setField(fieldError.getField());
+            error1.setMessage(fieldError.getDefaultMessage());
+            error1.setInvalidValue(error1.getInvalidValue());
+
+            errorList.add(error1);
         });
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorList(errorList);
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setCode("");
+        errorResponse.setMessage(httpServletRequest.getRequestURI());
+        errorResponse.setResultCode("FAIL");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     // @RequestParam의 @Validated 가 잘못되었을 때
     @ExceptionHandler(value = ConstraintViolationException.class)
-    public ResponseEntity constraintViolationException(ConstraintViolationException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+    public ResponseEntity constraintViolationException(ConstraintViolationException e, HttpServletRequest httpServletRequest){
+
+        List<Error> errorList = new ArrayList<>();
+
+        e.getConstraintViolations().forEach(error -> {
+            Stream<Path.Node> stream = StreamSupport.stream(error.getPropertyPath().spliterator(), false);
+            List<Path.Node> list = stream.collect(Collectors.toList());
+
+            String field = list.get(list.size() -1).getName();
+            String message = error.getMessage();
+            String invalidValue = error.getInvalidValue().toString();
+
+            Error error1 = new Error();
+            error1.setField(field);
+            error1.setMessage(message);
+            error1.setInvalidValue(invalidValue);
+
+            errorList.add(error1);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorList(errorList);
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setCode("");
+        errorResponse.setMessage(httpServletRequest.getRequestURI());
+        errorResponse.setResultCode("FAIL");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     // @RequestParam 이 누락되었을 때
     @ExceptionHandler(value = MissingServletRequestParameterException.class)
-    public ResponseEntity missingServletRequestParameterException(MissingServletRequestParameterException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+    public ResponseEntity missingServletRequestParameterException(MissingServletRequestParameterException e, HttpServletRequest httpServletRequest){
+
+        List<Error> errorList = new ArrayList<>();
+
+        Error error = new Error();
+        error.setField(e.getParameterName());
+        error.setMessage(e.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorList(errorList);
+        errorResponse.setMessage("");
+        errorResponse.setRequestUrl(httpServletRequest.getRequestURI());
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setResultCode("FAIL");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
